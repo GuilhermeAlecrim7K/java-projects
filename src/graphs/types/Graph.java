d3ad36cb9281a0435d5642d2d7ca232fabb54688
Guilherme Alecrim <guilhermealecrim96@gmail.com>
2022-04-24 21:34:04 -0300

    Behavior switched from GraphUtils to Graph

--- src/graphs/types/Graph.java
+++ src/graphs/types/Graph.java
@@ -3,13 +3,19 @@ package types;
 import java.util.Iterator;
 
 public interface Graph {
-	
+
 	Integer getNumberOfVertices();
-	
+
 	Integer getNumberOfEdges();
 
+	Integer getDegree(Integer vertex);
+
+	Integer maxDegree();
+
+	Double averageDegree(Graph graph);
+
 	void addEdge(Integer v, Integer w);
-	
+
 	boolean isValidEdge(Integer v, Integer w);
 
 	Iterator<Integer> getAdjacentVerticesTo(Integer v);
